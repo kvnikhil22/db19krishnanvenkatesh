66a7c30330b0ca4ed1b8bab33c115023772e94f7
kvnikhil22 <89282292+kvnikhil22@users.noreply.github.com>
2021-11-10 20:39:28 -0600

    Updated the files

--- app.js
+++ app.js
@@ -9,9 +9,27 @@ var usersRouter = require('./routes/users');
 var addmodsRouter = require('./routes/addmods');
 var animalRouter = require('./routes/animal');
 var selectorRouter = require('./routes/selector');
-
-
+var Costume = require('./models/costume');
+var resource = require('./routes/resource');
+var marvel = require('./routes/marvel');
 var app = express();
+
+const connectionString =
+process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+{useNewUrlParser: true,
+useUnifiedTopology: true});
+
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+recreateDB();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,7 +46,8 @@ app.use('/users', usersRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/animal', animalRouter);
 app.use('/selector', selectorRouter);
-
+app.use('/resource', resource)
+app.use('/marvel',marvel)
 
 
 // catch 404 and forward to error handler
@@ -48,3 +67,23 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+async function recreateDB(){
+  // Delete everything
+  await Costume.deleteMany();
+  let instance1 = new  Costume({costume_type:"ghost", size:'small',cost:26.4});
+  let instance2 = new  Costume({costume_type:"spider", size:'large',cost:67.4});
+  let instance3 = new Costume({costume_type:"badman", size:'medium',cost:66.4});
+  instance1.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("First object saved")
+  });
+  instance2.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("second object saved")
+  });
+  instance3.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("third object saved")
+    });
+ }
+ 
