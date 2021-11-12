77cbf503d3e59a57eac237ff841664196bb68fad
kvnikhil22 <89282292+kvnikhil22@users.noreply.github.com>
2021-11-12 12:26:41 -0600

    Updated lab_12

--- controllers/costume.js
+++ controllers/costume.js
@@ -39,17 +39,56 @@ exports.costume_create_post = async function(req, res) {
 exports.costume_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
-exports.costume_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id);
-};
+// Handle Costume update form on PUT. 
+exports.costume_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Costume.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.costume_type)  
+               toUpdate.costume_type = req.body.costume_type; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        if(req.body.size) toUpdate.size = req.body.size; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
+// VIEWS
+
+// Handle a show all view
+
 exports.costume_view_all_Page = async function(req, res) {
+
     try{
-    theCostumes = await Costume.find();
-    res.render('costumes', { title: 'Costume Search Results', results: theCostumes });
+
+        theCostumes = await Costume.find();
+
+        res.render('costumes', { title: 'Costume Search Results', results: theCostumes });
+
     }
+
     catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+
+        res.status(500);
+
+        res.send(`{"error": ${err}}`);
+
+    }  
+
+};
+   // for a specific Costume.
+exports.costume_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Costume.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
     }
    };
